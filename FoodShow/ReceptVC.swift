cfed7d67bbfa9761f47c041fc712efa67b7dd6bd
aigo880 <aigerim.ilip@mail.ru>
2020-12-26 01:33:25 +0600

    Storyboard
    - отредактировала сториборды
    - главная, избранные, калькулятор, рецепт
    - созданы VC

---
+++ FoodShow/ReceptVC.swift
@@ -0,0 +1,30 @@
+//
+//  ReceptVC.swift
+//  FoodShow
+//
+//  Created by Aigerim Ilipova on 26.12.2020.
+//  Copyright © 2020 TeamOfFour. All rights reserved.
+//
+
+import UIKit
+
+class ReceptVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
