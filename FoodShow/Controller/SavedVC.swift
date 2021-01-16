1c567bdd6fe09349516b37ef8b2745319a7dc220
Бекболат Азамат <azadev@MacBook-Pro-Bekbolat.local>
2021-01-16 22:47:10 +0600

    merged with emoty view

--- FoodShow/Controller/SavedVC.swift
+++ FoodShow/Controller/SavedVC.swift
@@ -124,21 +124,15 @@ extension SavedVC: UICollectionViewDelegate, UICollectionViewDataSource{
 }
 
 extension UICollectionView {
-    
-//    func setEmptyImage(){
-//
-//        let emptyImage = UIImageView(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
-//        self.backgroundView = emptyImage
-//    }
 
     func setEmptyMessage(_ message: String) {
         
         let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
         messageLabel.text = message
-        messageLabel.textColor = .black
+        messageLabel.textColor = .white
         messageLabel.numberOfLines = 0;
         messageLabel.textAlignment = .center;
-        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 22)
         messageLabel.sizeToFit()
 
         self.backgroundView = messageLabel;
